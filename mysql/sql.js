c7e4b65e7b564f2334b73eae2d561985e731bb5d
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-11-10 17:26:49 +0900

    first commit

---
+++ mysql/sql.js
@@ -0,0 +1,7 @@
+// CRUD
+module.exports = {
+  customerList: `select * from customers`,
+  customerInsert: `insert into customers set ?`,
+  customerUpdate: `update customers set ? where id=?`,
+  customerDelete: `delete from customers where id=?`,
+};
